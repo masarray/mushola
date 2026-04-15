54954f84d5f3418197a1c2de58be26a1428b11e1
masarray <ari.sulistiono@gmail.com>
2026-04-16 00:06:53 +0700

    Change base path in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/progress-nurturer-hub/",
+  base: "/mushola/",
   server: {
     host: "::",
     port: 8080,
